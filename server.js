bcefab47182ebbfbea02f8f3433925b42f9a1803
Maria Scharin <maria.scharin@comvalresearch.com>
2021-05-22 18:25:26 +0200

    fixing and doning

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+// Set a local variable populated by the environment variable $NAME
+const FAVORITE_FOOD = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  const favoriteFood = process.env.FAVORITE_FOOD;
-
   while(true) {
-    console.log('Containers rule!');
-    console.log(`My favorite junkfood is ${favoriteFood}`);
+    console.log(`${FAVORITE_FOOD} rocks!!!`);
     await sleep(5000);
   }
 }
